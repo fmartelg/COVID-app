6b9f8b97c1f9896a76666dabf890109b6a551afc
bozbilgin <bozbilgin@paradigm-soft.com>
2020-09-03 09:10:52 -0400

    9/3/2020 - Version ready to go out.

--- src/MyCovidApp/Startup.cs
+++ src/MyCovidApp/Startup.cs
@@ -25,6 +25,7 @@ namespace MyCovidApp
         {
             services.AddControllersWithViews();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
